872b8e6160304fce6581380df43e951dabd95172
Guest User <Guest@Epicodus-8B.local>
2018-03-19 20:57:58 -0700

    this will add project

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
-//Buisness Logic
-function Ticket(genre,time,age){
-  this.genre = genre;
-  this.time = time;
-  this.age = age;
+//Business Logic
+function Account(name,deposit){
+  this.name = name;
+  this.deposit = deposit;
 }
+function Deposit(inputtedDeposit,newDeposit){
+  return inputtedDeposit + newDeposit
+}
+
 //User Interface Logic
 $(document).ready(function(){
-  $("form#formal").submit(function(){
+  $("form#initial").submit(function(){
     event.preventDefault();
-    var inputGenre = $("#genre").val();
-    var inputTime = $("#time").val();
-    var inputAge =  $("#age").val();
-    var newPrice = new Ticket(inputGenre,inputTime,inputAge);
 
-    if(newPrice.genre === "Horror" || newPrice.time === "Afternoon" || newPrice.age === "Senior"){
-      $(".result").text("7$");
-    } else{
-        $(".result").text("9$");
-    };
+    var inputtedName = $("input#name").val();
+    var inputtedDeposit = parseInt($("input#deposit").val());
+
+    var newAccount = new Account(inputtedName, inputtedDeposit);
+
+    $("#result").append("<span class='result'>" + newAccount.name + newAccount.deposit +"</span>");
+
+
   });
+  $("form#balance").submit(function(){
+    event.preventDefault();
+
+    var newDeposit = parseInt($("input#deposit-amount").val());
+
+    var addAmount = Deposit(newDeposit);
+
+    alert(addAmount);
+
+
+  });
+
 });
